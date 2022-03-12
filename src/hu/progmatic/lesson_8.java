26b49b199d1e49e403cf29d391cf2c17fbd15a18
Moraq99 <kukucskamacska@gmail.com>
2022-03-12 15:51:58 +0100

    szokoeves feladat (nincs még kész)

--- src/hu/progmatic/lesson_8.java
+++ src/hu/progmatic/lesson_8.java
@@ -10,20 +10,24 @@ public class lesson_8 {
         //1582-ben vezették be (tehát azelőtti év biztosan nem szökőév)
         //azon évek szökőévek, amelyek oszthatóak 4-gyel
         //de a 100-zal osztható évek közül csak azok szökőévek, amelyek 400-zal is oszthatóak
-        int evszam = 1582;
+        //int evszam = 1582;
         System.out.println("Hello, adj egy évszámot, megmondom szökőév-e!");
+        int evszam = scanner.nextInt();
+        scanner.nextLine();
 
+        boolean nemszokoev = ((evszam % 4 !=0) && (evszam < 1582));
+        boolean szokoev = evszam % 4 ==0 && evszam > 1582;
 
-        for(int evszam =1584; evszam <=2020; evszam++){
-            if(szokoevE(evszam)) {
-                System.out.println("A(z) " + evszam + " -es év szökőév!");
-            else
-                System.out.println("A(z)  " + evszam + "  -es év NEM szökőév");
-            }
+        if(szokoev){
+            System.out.println("A(z) " + evszam + " -es év szökőév!");
+        } else if  (evszam % 400 == 0 ) {
+            System.out.println("A(z) " + evszam + " -es év szökőév!");
+        } else {
+            System.out.println("A(z)  " + evszam + "  -es év NEM szökőév!");
         }
 
-        public static boolean szokoevE(int evszam) {
-            return (evszam % 400 == 0) || ((evszam % 4 == 0) && (evszam % 100 != 0));
+
+
 
 
 
