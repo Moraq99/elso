104392c3db61665f3c1b13f1070e28e625fb0b2b
Moraq99 <kukucskamacska@gmail.com>
2022-03-12 17:49:38 +0100

    szokoeves feladat MOST MÁR JÓ ! :D v2

--- src/hu/progmatic/lesson_8.java
+++ src/hu/progmatic/lesson_8.java
@@ -11,7 +11,7 @@ public class lesson_8 {
         //azon évek szökőévek, amelyek oszthatóak 4-gyel
         //de a 100-zal osztható évek közül csak azok szökőévek, amelyek 400-zal is oszthatóak
         //int evszam = 1582;
-        System.out.println("Hello, adj egy évszámot, megmondom szökőév-e!");
+        /*System.out.println("Hello, adj egy évszámot, megmondom szökőév-e!");
         int evszam = sc.nextInt();
         sc.nextLine();
 
@@ -24,7 +24,52 @@ public class lesson_8 {
             System.out.println("A(z) " + evszam + " -es év szökőév!");
         } else {
             System.out.println("A(z)  " + evszam + "  -es év NEM szökőév!");
+        }*/
+
+        /*System.out.println("Kérem a számot és én megmondom, hogy prím szám-e!");
+        int szam = sc.nextInt();
+        sc.nextLine();
+
+
+
+        int i =2;
+        boolean prim = false;
+        //for (int i = 2; i <= szam/2;)
+        while ( i <= szam / 2) {
+            if (szam % i == 0) {
+                prim = true;
+                break;
+                //System.out.println("Nem Prím szám a " + szam + "!");
+            }
+            ++i;
         }
+            if (!prim) {
+                //} else if (){
+                //System.out.println("Nem Prím szám!");
+                System.out.println("A " + szam + " Prím szám!");
+            } else {
+                System.out.println("Nem Prím szám a " + szam + "!");
+            }*/
+
+        System.out.println("Kérem a számot!");
+        int szam = sc.nextInt();
+        sc.nextLine();
+        int db = szam;
+        int n = 0;
+        int[] osztok = new int[n];
+        for (int i = 1; i <=db;i++)
+            if (db % i == 0) {
+                System.out.println("A szám osztója " + i);
+                n++;
+            }
+        System.out.println("a szám osztóinak száma: " + n);
+        System.out.println();
+
+
+
+
+
+
 
 
 
